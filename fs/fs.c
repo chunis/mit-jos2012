709b8251d2af78e8f9b938b82628a8d0adb3085c
Chunis Deng <chunchengfh@gmail.com>
2012-05-29 15:13:28 +0800

    alloc_block() is done, and Ex5.3 is pass

--- fs/fs.c
+++ fs/fs.c
@@ -61,7 +61,14 @@ alloc_block(void)
 	// super->s_nblocks blocks in the disk altogether.
 
 	// LAB 5: Your code here.
-	panic("alloc_block not implemented");
+	int i = 2 + super->s_nblocks/BLKBITSIZE;
+
+	for (; i < super->s_nblocks; i++){
+		if(block_is_free(i)){
+			bitmap[i/32] &= ~(1<<(i%32));
+			return i;
+		}
+	}
 	return -E_NO_DISK;
 }
 
