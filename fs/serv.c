8aa6878fd997077f7c8ad277d492d5ab97ddacbf
Chunis Deng <chunchengfh@gmail.com>
2012-05-30 15:01:40 +0800

    Ex5.6 is pass

--- fs/serv.c
+++ fs/serv.c
@@ -240,11 +240,27 @@ serve_read(envid_t envid, union Fsipc *ipc)
 int
 serve_write(envid_t envid, struct Fsreq_write *req)
 {
+	struct OpenFile *o;
+	int r;
+	int n = req->req_n;
+
 	if (debug)
 		cprintf("serve_write %08x %08x %08x\n", envid, req->req_fileid, req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	if(n > sizeof(req->req_buf))
+		n = sizeof(req->req_buf);
+
+	if ((r = openfile_lookup(envid, req->req_fileid, &o)) < 0)
+		return r;
+
+	r = file_write(o->o_file, req->req_buf, n, o->o_fd->fd_offset);
+	if(r < 0)
+		return r;
+
+	// update fd_offset
+	o->o_fd->fd_offset += r;
+	return r;
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
